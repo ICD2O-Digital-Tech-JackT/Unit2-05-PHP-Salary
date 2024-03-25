76a9816136c4cc6f0b1db12ae3da1013e262e352
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-03-25 15:05:10 +0000

    began

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+    <?php
+    $Wage = 15.50;
+    $Hours = 6;
+    echo "<h1>Calculating Salary in PHP</h1>";
+    echo "Your Salary is $Wage$ per hour";
+    echo "<br><br>";
+    echo "You work $Hours hours a day";
+    echo "<br><br>";
+    ?>
+    
+  </body>
+</html>
